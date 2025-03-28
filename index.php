c41d717d460e7d4e405c6bfc44f321b32a89dd98
stocky777 <shivlymizi@gmail.com>
2025-03-28 02:34:08 +0000

    working timetable, added Tuhin's website, issue with the responsiveness on smaller screens

--- index.php
+++ index.php
@@ -1,53 +1,44 @@
-<?php
-
-
+<?php include 'classes/includes/header.php'; 
 ?>
+<div class="container my-5">
+  <h1>Timetables</h1>
+  <p>Prayer times and community event schedules.</p>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/stylesheet.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>timetable</title>
-    <div class="container-fluid">
-        <?php
-        // #error handlers
-        // if(json_last_error() !== JSON_ERROR_NONE)
-        // {
-        //     echo json_last_error_msg();
-        // }else{
-        //     $data = json_decode($response, flags: JSON_THROW_ON_ERROR);
-        //     // echo "<pre>";
-        //     // print_r($data);
-        //     // echo "</pre>";
-
-        //     foreach ($data->data as $day) 
-        //     {
-        //         echo "Date: " .$day->date->readable . "\n";
-        //         foreach($day->timings as $prayer => $time){
-                    
-        //             echo "$prayer: $time \n";
-        //         }
-        //     }
-        include("script.php");
-            
-
-            
-            //$items = $data->data[0]->timings;
-            
-            //print_r($items);}
-
-        
-
-
-        ?>
-    </div>
-</head>
-<body>
-    
-
-
-</body>
-</html>
+  <?php 
+    include_once("script.php");
+    $timetable = new PrayerTimeTable();
+    $ttbl = $timetable->generateTimetableMonth();
+  ?>
+  <!-- Example Timetable Table -->
+  <!-- <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Prayer</th>
+        <th scope="col">Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Fajr</td>
+        <td>05:00 AM</td>
+      </tr>
+      <tr>
+        <td>Dhuhr</td>
+        <td>12:30 PM</td>
+      </tr>
+      <tr>
+        <td>Asr</td>
+        <td>04:15 PM</td>
+      </tr>
+      <tr>
+        <td>Maghrib</td>
+        <td>06:45 PM</td>
+      </tr>
+      <tr>
+        <td>Isha</td>
+        <td>08:00 PM</td>
+      </tr>
+    </tbody>
+  </table> -->
+</div>
+<!-- <?php include 'includes/footer.php'; ?> -->
