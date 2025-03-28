4d983aabe41e6a76cd7c0068a2918737d47ff7e9
stocky777 <shivlymizi@gmail.com>
2025-03-28 00:13:52 +0000

    changing date according to the current month has been fixed

--- script.php
+++ script.php
@@ -6,22 +6,26 @@ class PrayerTimetable {
     //data of the api from the response will be stored here
     private $data;
     //pasing the url of the json data here directly
-    private $api_url = "https://api.aladhan.com/v1/calendar/2025/3?latitude=56.47404281844331&longitude=-2.963501315403592&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ";
+    private $api_url = "https://api.aladhan.com/v1/calendar/{year}/{month}?latitude=56.47404281844331&longitude=-2.963501315403592&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ";
     //construct class when called will automatically call the api aswell and therefore decode it
+    
     public function __construct()
     {
         //check response of the content
         #$this->response = file_get_contents($this->api_url);
+
+        //need to implement a function that changes the year and month
+        $tmp = $this->checkdateandchange();
         //checks if there's anything wrong with the json data before displaying
-        $this->response = $this->checkIntegrityOfData();
+        $this->response = $this->checkIntegrityOfData($tmp);
         //decodes the json data
         $this->data = json_decode($this->response);
     }
 
     //checking if the data is working properly if not it will send a message instead
-    public function checkIntegrityOfData()
+    public function checkIntegrityOfData($item)
     {
-        $txt = file_get_contents($this->api_url);
+        $txt = file_get_contents($item);
         if($txt == FALSE)
         {
             echo "<h1>something is wrong please retry again later</h1> <br>";
@@ -33,13 +37,16 @@ class PrayerTimetable {
 
     //nearly done need to change a few things for it
     public function checkdateandchange(){
-        echo $this->api_url;
-        echo '<br>--------------------------------------------- </br>';
-        //echo str_replace("2025","2024",$this->api_url);
+        //echo $this->api_url;
+        //echo '<br>--------------------------------------------- </br>';
+        $tmp = $this->api_url;
         $thismonth = date('n');
         $thisyear = date('Y');
-        echo $thismonth, $thisyear;
-        echo '<br>--------------------------------------------- </br>';
+
+        $updt_api_url = str_replace(["{year}","{month}"],[$thisyear,$thismonth],$tmp);
+        return $updt_api_url;
+        //echo $thismonth, $thisyear;
+        //echo '<br>--------------------------------------------- </br>';
     }
 
     //function doesnt't store the data due to database memory not being enough
@@ -80,7 +87,6 @@ class PrayerTimetable {
 $timetable = new PrayerTimeTable();
 $test = $timetable->checkdateandchange();
 //$ttbl = $timetable->getTimetable();
-
 
         
 
