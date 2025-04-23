d52faf52cb9b551e0e766b3745336617b9c2e670
stocky777 <shivlymizi@gmail.com>
2025-04-23 20:24:57 +0100

    changes of files and working displaying of news

--- main/news.php
+++ main/news.php
@@ -4,14 +4,11 @@
   <p>Stay updated with the latest news and community announcements.</p>
   <!-- Example News Items -->
   <div class="list-group">
-    <a href="#" class="list-group-item list-group-item-action">
-      <h5 class="mb-1">News Item 1</h5>
-      <p class="mb-1">Summary of news item 1.</p>
-    </a>
-    <a href="#" class="list-group-item list-group-item-action">
-      <h5 class="mb-1">News Item 2</h5>
-      <p class="mb-1">Summary of news item 2.</p>
-    </a>
+    <?php include 'database.php'; ?>
   </div>
 </div>
-<?php include 'includes/footer.php'; ?>
+<?php 
+
+
+
+include 'includes/footer.php'; ?>
